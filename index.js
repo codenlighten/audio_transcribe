ebf0c37f65bb2c1fd550e419c9dfef9c875ef7e3
greg ward <codenlighten1@gmail.com>
2024-02-08 08:30:11 -0500

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); // Serve your static HTML file
+
+app.post("/api/transcription", (req, res) => {
+  const { timestamp, transcription } = req.body;
+  const newNote = {
+    timestamp,
+    transcription,
+    id: uuidv4(),
+  };
+  const notes = JSON.parse(fs.readFileSync("db/db.json"));
+  notes.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(notes));
+  res.json(newNote);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
