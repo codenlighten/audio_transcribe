252ae7ce8c2d088effbbae6240a5b482a1183bf6
greg ward <codenlighten1@gmail.com>
2024-02-08 08:50:37 -0500

    first

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-require("dotenv").config();
-const express = require("express");
 const fs = require("fs");
+const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); // Serve your static HTML file
+
+// Create a middleware to check if db.json exists, if not, create an empty array
+app.use((req, res, next) => {
+  const dbPath = "db/db.json";
+  if (!fs.existsSync(dbPath)) {
+    fs.writeFileSync(dbPath, "[]");
+  }
+  next();
+});
 
 app.post("/api/transcription", (req, res) => {
   const { timestamp, transcription } = req.body;
